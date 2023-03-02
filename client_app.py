bac6b488e3b0d0269604cddaddd9231799756873
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-03-01 22:35:10 -0800

    create_tlp_refactoring

--- client_app.py
+++ client_app.py
@@ -1,13 +1,25 @@
 import asyncio
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="py_log_client.log",filemode="w")
 
 math_expression = input('Введите математическое выражение: ')
-async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 8888)
-    print(f'Математическое выражение: {message}')
-    writer.write(message.encode())
-    data = await reader.read(100)
-    print(f'Результат выражения: {data.decode()}')
-    writer.close()
+dict_math_json = {'expressions': math_expression}
 
-asyncio.run(tcp_echo_client(math_expression))
+async def tcp_echo_client(message):
+    try:
+        reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+        logging.info("Соединение с сервером успешно")
+        print(f'Математическое выражение: {dict_math_json["expressions"]}')
+        json_message = json.dumps(message).encode()
+        writer.write(json_message)
+        request = await reader.read(100)
+        request = json.loads(request.decode())
+        print(f'Результат выражения: {request["answer"]}')
+        writer.close()
+    except ConnectionRefusedError:
+        logging.critical('Соеденение с сервером не установлено')
+
+asyncio.run(tcp_echo_client(dict_math_json))
--- server_app.py
+++ server_app.py
@@ -1,25 +1,38 @@
 import asyncio
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="py_log_server.log", filemode="w")
 
 async def handle_echo(reader, writer):
-    request = (await reader.read(255)).decode('utf8')
+    request = (await reader.read(100))
+    request = json.loads(request.decode())
     try:
-        response = str(eval(request))
-        writer.write(response.encode('utf8'))
+        expressions_response = str(eval(request["expressions"]))
+        response_json = {"answer": expressions_response}
+        response_json_answer = json.dumps(response_json).encode()
+        writer.write(response_json_answer)
         await writer.drain()
         writer.close()
-    except BaseException:
-        response = request + ' невалидное выражение'
-        writer.write(response.encode('utf8'))
+    except NameError:
+
+        response_json = {"answer": request["expressions"] + ' invalid expression'}
+        response_json_answer = json.dumps(response_json).encode('utf8')
+        writer.write(response_json_answer)
         await writer.drain()
         writer.close()
 
 async def main():
-    server = await asyncio.start_server(
-        handle_echo, '127.0.0.1', 8888)
-    addr = server.sockets[0].getsockname()
-    print(f'Host server: {addr[0]}'
-          f'\nPort server: {addr[1]}')
-    async with server:
-        await server.serve_forever()
+    try:
+        logging.info('Сервер работает корректно')
+        server = await asyncio.start_server(
+            handle_echo, '127.0.0.1', 8888)
+        addr = server.sockets[0].getsockname()
+        print(f'Host server: {addr[0]}'
+              f'\nPort server: {addr[1]}')
+        async with server:
+            await server.serve_forever()
+    except ConnectionRefusedError:
+        logging.critical('Сервер не работает!')
 
 asyncio.run(main())
